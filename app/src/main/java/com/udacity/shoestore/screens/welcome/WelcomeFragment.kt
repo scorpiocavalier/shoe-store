fb6be31b9178594b4f7b2a4250ad2fd539f9d7ba
Arturo Chen <art.can.fly@gmail.com>
2020-12-11 02:55:51 -0500

    + databinding

--- app/src/main/java/com/udacity/shoestore/screens/welcome/WelcomeFragment.kt
+++ app/src/main/java/com/udacity/shoestore/screens/welcome/WelcomeFragment.kt
@@ -4,24 +4,37 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.Fragment
 import androidx.navigation.Navigation
 import com.udacity.shoestore.R
-import kotlinx.android.synthetic.main.fragment_welcome.*
+import com.udacity.shoestore.databinding.FragmentWelcomeBinding
 
 class WelcomeFragment : Fragment() {
+
+  private lateinit var binding: FragmentWelcomeBinding
+  private val welcomeText = "Welcome to the ShoeStore"
+  private val instructionsText = "Read the instructions on how to use the app"
+  private val buttonText = "Instructions"
 
   override fun onCreateView(
     inflater: LayoutInflater, container: ViewGroup?,
     savedInstanceState: Bundle?
-  ): View? {
-    return inflater.inflate(R.layout.fragment_welcome, container, false)
+  ): View {
+
+    binding = DataBindingUtil.inflate(inflater, R.layout.fragment_welcome, container, false)
+
+    binding.welcomeText = welcomeText
+    binding.instructionsText = instructionsText
+    binding.buttonText = buttonText
+
+    return binding.root
   }
 
   override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
     super.onViewCreated(view, savedInstanceState)
 
-    instructions_button.setOnClickListener(
+    binding.instructionsButton.setOnClickListener(
       Navigation.createNavigateOnClickListener(
         WelcomeFragmentDirections.actionWelcomeFragmentToInstructionsFragment()
       )
