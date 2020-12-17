14c117f663fa5c61de761bac042ba47a939ff708
Arturo Chen <art.can.fly@gmail.com>
2020-12-17 07:16:32 -0500

    * Menu only shows in ShoeListFragment.kt

--- app/src/main/java/com/udacity/shoestore/MainActivity.kt
+++ app/src/main/java/com/udacity/shoestore/MainActivity.kt
@@ -1,39 +1,12 @@
 package com.udacity.shoestore
 
 import android.os.Bundle
-import android.view.Menu
-import android.view.MenuItem
 import androidx.appcompat.app.AppCompatActivity
-import androidx.databinding.DataBindingUtil
-import androidx.navigation.NavController
-import androidx.navigation.ui.NavigationUI
-import com.udacity.shoestore.databinding.ActivityMainBinding
-import timber.log.Timber
 
 class MainActivity : AppCompatActivity() {
 
-  private lateinit var binding: ActivityMainBinding
-  private lateinit var navController: NavController
-
   override fun onCreate(savedInstanceState: Bundle?) {
     super.onCreate(savedInstanceState)
-    binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
-
-    // In MainActivity, setup the nav controller with the toolbar and an AppBarConfiguration.
-    navController = NavController(this)
-//    NavigationUI.setupActionBarWithNavController(this, navController)
-
-    Timber.plant(Timber.DebugTree())
-  }
-
-  // Add Log out menu
-  override fun onCreateOptionsMenu(menu: Menu?): Boolean {
-    menuInflater.inflate(R.menu.overflow_menu, menu)
-    return true
-  }
-
-  override fun onOptionsItemSelected(item: MenuItem): Boolean {
-    return NavigationUI.onNavDestinationSelected(item, navController)
-            || super.onOptionsItemSelected(item)
+    setContentView(R.layout.activity_main)
   }
 }
